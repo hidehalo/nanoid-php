a3cd994b4c6fdc99cd27add4ab147952fdf9731b
hidehalo <tianchen_cc@yeah.net>
2017-09-16 21:22:28 +0800

    Benchmark: Add randome generator benchmark script

---
+++ benchmark/benchmark.php
@@ -0,0 +1,24 @@
+<?php
+require __DIR__.'/../vendor/autoload.php';
+
+use Hidehalo\Nanoid\Client;
+
+$nano = new Client();
+$loop = 1e5;
+$alphabet = 'abcdefghijklmnopqrstuvwxyz';
+$counter = [];
+
+$timeStated = microtime(true);
+for ($i = 0; $i < $loop; $i++) {
+    $alpha = $nano->formatedId($alphabet, 1);
+    !isset($counter[$alpha]) and $counter[$alpha] = 0;
+    isset($counter[$alpha]) and $counter[$alpha]++;
+}
+$timeFinished = microtime(true);
+$deltaTime = $timeFinished - $timeStated;
+
+ksort($counter);
+printf("Total time used: %.3f ms\n", 1e3 * $deltaTime);
+printf("AVG time used: %.3f ms\n", 1e3 * $deltaTime/$loop);
+echo "Result:".PHP_EOL;
+print_r($counter);
