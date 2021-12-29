d7be878e8f33b49f12a33454afd22777790b9e54
Delonn Koh <delonnkoh.dev@gmail.com>
2021-12-29 21:57:41 +0800

    fix: Normal mode now generate random id correctly

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -17,11 +17,13 @@ class ClientTest extends TestCase
         $size = 7;
         $normalRandom = $client->generateId($size);
         $this->assertEquals($size, strlen($normalRandom));
+        $this->assertNotEquals(str_repeat('_', $size), $normalRandom);
         $dynamicRandom = $client->generateId($size, Client::MODE_DYNAMIC);
         $this->assertEquals($size, strlen($dynamicRandom));
         $this->assertNotEquals($normalRandom, $dynamicRandom);
         $defaultRandom = $client->generateId();
         $this->assertEquals(21, strlen($defaultRandom));
+        $this->assertNotEquals(str_repeat('_',21), $normalRandom);
     }
 
     /**
